59e602979e23dd5163de43689235836e0f243877
Giada-Z <jz2733@columbia.edu>
2016-12-22 16:32:44 -0500

    Update network.py

--- network.py
+++ network.py
@@ -9,13 +9,14 @@ from pattern.en import singularize, pluralize
 
 nodeDF = pd.read_csv("/Users/Jiajia/Google Drive/Columbia/Big Data/Nodes.csv", header=None, names=['ingredients'])
 recDF = pd.read_csv("/Users/Jiajia/Google Drive/Columbia/Big Data/Clean_recipes.csv", header=None, names=['ingredients'])
-n = nodeDF.count()['ingredients']    ## n = 2174
+n = nodeDF.count()['ingredients']    ## n = 2148
 m = recDF.count()['ingredients']    ## m = 27341
-prob_ingr = np.zeros(n)
+prob = np.zeros(n)
 
-## Compute probability of each ingredient
 recDF['ingredients'] = recDF['ingredients'].str.lower()
 
+## Record recipes where each ingredient presents
+## Compute probability of each ingredient
 location = []
 for i, ingr in nodeDF.iterrows():
 	count = 0
@@ -27,13 +28,8 @@ for i, ingr in nodeDF.iterrows():
 	if count == 0:
 		print "Error: %d, %s" % (i, ingr)
 	else:
-		prob_ingr[i] = count*1.0 / m
+		prob[i] = count*1.0 / m
 		location.append(occur)
-
-with open('prob.csv', 'wb') as myfile:
-	wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-	for prob in prob_ingr:
-		wr.writerow([prob])
 
 with open('location.csv', 'wb') as myfile:
 	wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
@@ -41,18 +37,12 @@ with open('location.csv', 'wb') as myfile:
 		wr.writerow([loc])
 
 
-## Compute PMI and Generate Edges, threshold = 0.05
+## Compute PMI and Generate Edges, threshold = 0.3
 position = []
 with open('/Users/Jiajia/Google Drive/Columbia/Big Data/location.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:       # each row is read as a list
     	position.append(literal_eval(row[0]))
-
-prob = []
-with open('/Users/Jiajia/Google Drive/Columbia/Big Data/prob.csv', 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
-    	prob.append(Decimal(row[0]))
 
 PMI = np.empty([n, n])
 edges = []
@@ -63,7 +53,7 @@ for i in range(n-1):
 		if jointprob != 0:
 			PMI[i][j] = log(jointprob/(prob[i] * prob[j]))
 			PMI[j][i] = PMI[i][j]
-			if PMI[i][j] > 0.05:		
+			if PMI[i][j] > 0.3:		
 				edges.append({'source':i, 'target':j})
 for k in range(n):
 	PMI[k][k] = -log(prob[k])
